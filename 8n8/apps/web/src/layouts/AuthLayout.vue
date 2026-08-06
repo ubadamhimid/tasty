b1153cfac5f1ed4e08dcca0b5ed499199131dbd4
ubadamhimid <ubadara.101@gmail.com>
2026-08-07 00:15:38 +0200

    Clean repository and isolate tasty project

--- 8n8/apps/web/src/layouts/AuthLayout.vue
+++
@@ -1,9 +0,0 @@
-<template>
-  <div class="min-h-screen bg-dark-900 flex items-center justify-center p-4">
-    <slot></slot>
-  </div>
-</template>
-
-<script setup lang="ts">
-// Simple centered layout for authentication pages
-</script>
